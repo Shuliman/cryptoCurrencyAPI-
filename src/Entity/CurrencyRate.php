bca17b52dcbb2682ff5492bd4f7eb8be1c041884
Shuliman <Julik.schuldiner@proton.me>
2023-12-06 15:18:10 +0100

    created model and migration for currency_rates

---
+++ src/Entity/CurrencyRate.php
@@ -0,0 +1,102 @@
+<?php
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="currency_rates")
+ */
+class CurrencyRate
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /** @ORM\Column(type="integer") */
+    private $time;
+
+    /** @ORM\Column(type="decimal", precision=16, scale=8) */
+    private $high;
+
+    /** @ORM\Column(type="decimal", precision=16, scale=8) */
+    private $low;
+
+    /** @ORM\Column(type="decimal", precision=16, scale=8) */
+    private $open;
+
+    /** @ORM\Column(type="decimal", precision=16, scale=8) */
+    private $close;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTime(): ?int
+    {
+        return $this->time;
+    }
+
+    public function setTime(int $time): self
+    {
+        $this->time = $time;
+        return $this;
+    }
+
+    public function getFormattedTime(): string
+    {
+        return date('Y-m-d H:i:s', $this->time);
+    }
+
+    public function setFormattedTime(string $formattedTime): self
+    {
+        $this->time = strtotime($formattedTime);
+        return $this;
+    }
+
+    public function getHigh(): string
+    {
+        return $this->high;
+    }
+
+    public function setHigh(string $high): self
+    {
+        $this->high = $high;
+        return $this;
+    }
+
+    public function getLow(): string
+    {
+        return $this->low;
+    }
+
+    public function setLow(string $low): self
+    {
+        $this->low = $low;
+        return $this;
+    }
+
+    public function getOpen(): string
+    {
+        return $this->open;
+    }
+
+    public function setOpen(string $open): self
+    {
+        $this->open = $open;
+        return $this;
+    }
+
+    public function getClose(): string
+    {
+        return $this->close;
+    }
+
+    public function setClose(string $close): self
+    {
+        $this->close = $close;
+        return $this;
+    }
+}
