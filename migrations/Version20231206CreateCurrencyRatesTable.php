224bab8676c32c0f1de963db2901256ce6a512d5
Shuliman <Julik.schuldiner@proton.me>
2023-12-06 15:18:10 +0100

    created model and migration for currency_rates

---
+++ migrations/Version20231206CreateCurrencyRatesTable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20231206CreateCurrencyRatesTable extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $table = $schema->createTable('currency_rates');
+
+        $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('time', 'integer');
+        $table->addColumn('high', 'decimal', ['precision' => 16, 'scale' => 8]);
+        $table->addColumn('low', 'decimal', ['precision' => 16, 'scale' => 8]);
+        $table->addColumn('open', 'decimal', ['precision' => 16, 'scale' => 8]);
+        $table->addColumn('close', 'decimal', ['precision' => 16, 'scale' => 8]);
+
+        $table->setPrimaryKey(['id']);
+
+        $table->addIndex(['time'], 'time_idx');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $schema->dropTable('currency_rates');
+    }
+}
