5cb95803c0cd4ec2a302629a65dc1d6946c728f7
Shuliman <Julik.schuldiner@proton.me>
2023-12-07 11:38:00 +0100

    Added currency_pair value into DB

--- migrations/Version20231206CreateCurrencyRatesTable.php
+++ migrations/Version20231206CreateCurrencyRatesTable.php
@@ -12,6 +12,7 @@ final class Version20231206CreateCurrencyRatesTable extends AbstractMigration
         $table = $schema->createTable('currency_rates');
 
         $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('currency_pair', 'string', ['length' => 7]);
         $table->addColumn('time', 'integer');
         $table->addColumn('high', 'decimal', ['precision' => 16, 'scale' => 8]);
         $table->addColumn('low', 'decimal', ['precision' => 16, 'scale' => 8]);
@@ -21,6 +22,7 @@ final class Version20231206CreateCurrencyRatesTable extends AbstractMigration
 
         $table->setPrimaryKey(['id']);
 
+        $table->addIndex(['currency_pair'], 'currency_pair_idx');
         $table->addIndex(['time'], 'time_idx');
     }
 
