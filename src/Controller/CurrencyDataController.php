2d11300615a5c42da19de7bab09eb22749ad24a8
Shuliman <Julik.schuldiner@proton.me>
2023-12-10 14:26:23 +0100

    Added annotations

--- src/Controller/CurrencyDataController.php
+++ src/Controller/CurrencyDataController.php
@@ -7,15 +7,30 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Controller for handling currency data requests.
+ */
 class CurrencyDataController extends AbstractController
 {
     private $currencyDataManagementService;
 
+    /**
+     * Constructor of the controller.
+     *
+     * @param CurrencyDataManagementService $currencyDataManagementService Service for managing currency data.
+     */
     public function __construct(CurrencyDataManagementService $currencyDataManagementService)
     {
         $this->currencyDataManagementService = $currencyDataManagementService;
     }
 
+    /**
+     * Fetches hourly currency data.
+     *
+     * @Route('/currency/hourly', methods: ['GET'])
+     * @param Request $request The current request object.
+     * @return JsonResponse The response containing currency data or error message.
+     */
     #[Route('/currency/hourly', methods: ['GET'])]
     public function getHourlyData(Request $request): JsonResponse
     {
@@ -41,7 +56,16 @@ class CurrencyDataController extends AbstractController
         }
     }
 
-
+    /**
+     * Fetches daily currency data.
+     *
+     * @Route('/currency/daily', methods: ['GET'])
+     * @param Request $request The current request object.
+     * @return JsonResponse The response containing daily currency data or an error message.
+     *
+     * Retrieves currency data for a specific day or the current day if no specific date is provided.
+     * The start and end times are set to cover the entire day.
+     */
     #[Route('/currency/daily', methods: ['GET'])]
     public function getDailyData(Request $request): JsonResponse
     {
@@ -61,7 +85,16 @@ class CurrencyDataController extends AbstractController
             return $this->json(['success' => false, 'message' => $e->getMessage()]);
         }
     }
-
+    /**
+     * Fetches weekly currency data.
+     *
+     * @Route('/currency/weekly', methods: ['GET'])
+     * @param Request $request The current request object.
+     * @return JsonResponse The response containing weekly currency data or an error message.
+     *
+     * Retrieves currency data for a specific week, starting from the specified date or the current date if none is provided.
+     * The data covers a seven-day period starting three days before the specified date.
+     */
     #[Route('/currency/weekly', methods: ['GET'])]
     public function getWeeklyData(Request $request): JsonResponse
     {
